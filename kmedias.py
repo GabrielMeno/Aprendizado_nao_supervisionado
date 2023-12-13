5219b54e23f503dcf43363f0681a44a33f2ea7ec
GabrielMeno <gabriel.menoncin@estudante.uffs.edu.br>
2023-12-13 01:57:34 -0300

    pog

--- kmedias.py
+++ kmedias.py
@@ -44,15 +44,13 @@ def contagem(img):
     )
     return counts.size
 def simage(img, name, clusters, output_id=0):
-    # Convertendo a imagem de RGB para BGR
+
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    # Cria a pasta de saída, caso não exista
     folder = f'./outputs/{output_id}'
     if not os.path.exists(folder):
         os.makedirs(folder)
-
-    # Salva a imagem
+        
     path = f'{folder}/{name[:-4]}_{clusters}.png'
     if not cv2.imwrite(path, img):
         raise Exception('Erro ao salvar imagem')
@@ -62,8 +60,6 @@ def tamanho(path):
     return os.path.getsize(path) / 1000000
 def setup_logger(output_id):
     log_path = f'./outputs/{output_id}/log.txt'
-
-    # Caso não exista, cria o arquivo de log
     if not os.path.exists(os.path.dirname(log_path)):
         os.makedirs(os.path.dirname(log_path))
 
@@ -73,8 +69,6 @@ def setup_logger(output_id):
         format='%(message)s',
         encoding='utf-8',
     )
-
-    # Seta o nível de log para INFO e adiciona um handler para imprimir no console
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
@@ -112,49 +106,36 @@ def get_last_output():
     return max([int(f) for f in os.listdir('outputs') if f.isdigit()] + [0])
 def output_exists(output_id, image, clusters):
     return os.path.exists(f'./outputs/{output_id}/{image[:-4]}_{clusters}.png')
-if __name__ == '__main__':
+if __name__ == 'main':
 
-    # Parâmetros de execução
     mode = MODE.RESTART
     sizes = [1, 3, 5, 9, 13, 19, 23]
-
-
+    
     images = get_images()
     last_output = get_last_output()
 
-    # Verifica, caso o modo seja CONTINUE, se todos os outputs já foram gerados
     if mode == MODE.CONTINUE and last_output > 0:
         if len(get_output_images(last_output)) >= len(images) * len(sizes):
             mode = MODE.RESTART
 
     output_id = last_output + 1 if mode == MODE.RESTART else last_output
-
+    
     setup_logger(output_id)
 
     for image_name in images:
         path = f'./images/{image_name}'
-
-        # Faz o log inicial da imagem
         if mode == MODE.RESTART or not output_exists(output_id, image_name, sizes[0]):
             print_initial_info(image_name)
             print_info(path, resolution=True)
 
-        # Salva o tamanho original da imagem para calcular a compressão
         original_size = tamanho(path)
 
         for n in sizes:
-
-            # Pula a execução caso o output já exista e o modo seja CONTINUE
             if mode == MODE.CONTINUE and output_exists(output_id, image_name, n):
                 continue
-
-            # Executa o algoritmo e calcula o tempo de execução
             start = time.time()
             new_image = kmeans(path, n)
             timer = time.time() - start
-
-            # Salva a imagem e faz o log final da execução
             output = simage(new_image, image_name, n, output_id)
             print_info(output, timer=timer, original_size=original_size)
             mimagem(new_image)
-
